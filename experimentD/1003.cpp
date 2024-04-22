dc42ccaa4f9b1e17d631fdf2152106ed3f4c443c
yizhilsy <1937714332@qq.com>
2024-04-22 14:17:34 +0800

    LL(1)文法语法检测表达式，条件表达式

--- experimentD/1003.cpp
+++ experimentD/1003.cpp
@@ -78,8 +78,9 @@ void compilerWork();
 void extractingIdentifiers(string& str);
 
 // 语法分析
-void ll1SyntaxAnalysis(string& str);    // LL(1) syntax analysis
+bool ll1SyntaxAnalysis(string& str);    // LL(1) expression syntax analysis
 void errorSearch(const deque<char>& synataxStr);
+bool ll1ConditionSyntaxAnalysis(string& str);
 
 int main()
 {
@@ -108,11 +109,24 @@ int main()
         }
     }
     
-
     // for(const auto& item : ansVec){
     //     outputFile<<"("<<item.first<<",    "<<item.second<<")"<<endl;
     // }
     // ll1SyntaxAnalysis(compileFileStr);
+
+    cout << "*****Condition Syntax Test*****" << endl;
+    // 打开文件
+    std::ifstream file("CompilerTheory\\experimentD\\synataxAnalysis02.txt");
+    // 读取文件内容到一个 std::string 对象中
+    std::string content02((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());    
+    // 关闭文件
+    file.close();
+    // 将待编译文件全部转为小写字符
+    transform(content02.begin(), content02.end(), content02.begin(), ::tolower);
+    cout << content02 << endl;
+    extractingIdentifiers(content02);
+    ll1ConditionSyntaxAnalysis(content02);
+
     return 0;
 }
 
@@ -235,9 +249,9 @@ void extractingIdentifiers(string& str) {
     }
 }
 
-void ll1SyntaxAnalysis(string& str) {
+bool ll1SyntaxAnalysis(string& str) {
     //处理分析一行的语法
-    //构建剩余串
+    //构建剩余串,并且检查是否有非法字符
     deque<char> synataxStr;
     for (const auto& data : ansVec) {
         if (data.first == "number") {
@@ -264,13 +278,15 @@ void ll1SyntaxAnalysis(string& str) {
         else if (data.second == "/") {
             synataxStr.push_back('/');
         }
+        else {  //此时是非法字符
+            cout << "Invaild symbol!" << endl;
+            return false;
+        }
     }
     deque<char> tempStr = synataxStr;
     synataxStr.push_back('#');
 
-
-        
-        //构建预测分析表
+    //构建预测分析表
     unordered_map<char, int> hashmap = {
         {'b',1},{'n',2},{'(',3},{')',4},{'+',5},{'-',6},{'*',7},{'/',8},{'#',9},
         {'E',1},{'R',2},{'I',3},{'O',4},{'F',5},{'A',6},{'M',7}
@@ -280,8 +296,8 @@ void ll1SyntaxAnalysis(string& str) {
         'b','n','(',')','+','-','*','/','#'
     };
     string predictTable[16][16];
-    for (int i = 1;i <= 9;i++) {
-        for (int j = 1;j <= 7;j++) {
+    for (int i = 1;i <= 7;i++) {
+        for (int j = 1;j <= 9;j++) {
             predictTable[i][j] = "ERROR";
         }
     }
@@ -311,7 +327,7 @@ void ll1SyntaxAnalysis(string& str) {
             cout << "终结符不一致" << endl;
             cout << "Synatax Error, Please Check." << endl;
             errorSearch(tempStr);
-            return;
+            return false;
         }
         else {  //从分析预测表中取出推理字符串
             string expression = predictTable[hashmap[stCh]][hashmap[strCh]];
@@ -320,7 +336,7 @@ void ll1SyntaxAnalysis(string& str) {
                 cout << "空预测表" << endl;
                 cout << "Synatax Error, Please Check." << endl;
                 errorSearch(tempStr);
-                return;
+                return false;
             }
             else if (expression == "ε") {   //处理产生式右部是空的情况
                 synataxSt.pop();
@@ -343,13 +359,13 @@ void ll1SyntaxAnalysis(string& str) {
             cout << "预测分析栈先空了" << endl;
             cout << "Synatax Error, Please Check." << endl;
             errorSearch(tempStr);
-            return;
+            return false;
         }
         
 
     }
     cout << "Synatax is Correct." << endl;
-
+    return true;
 }
 
 // 错误检查
@@ -401,6 +417,195 @@ void errorSearch(const deque<char>& synataxStr) {
     }
 
 }
+
+bool ll1ConditionSyntaxAnalysis(string& str) {
+    //处理分析一行的语法
+    //构建剩余串,并且检查是否有非法字符
+    deque<string> synataxStr;
+    for (const auto& data : ansVec) {
+        if (data.first == "number") {
+            synataxStr.push_back("n");
+        }
+        else if (data.first == "ident") {
+            synataxStr.push_back("b");
+        }
+        else if (data.second == "(") {
+            synataxStr.push_back("(");
+        }
+        else if (data.second == ")") {
+            synataxStr.push_back(")");
+        }
+        else if (data.second == "+") {
+            synataxStr.push_back("+");
+        }
+        else if (data.second == "-") {
+            synataxStr.push_back("-");
+        }
+        else if (data.second == "*") {
+            synataxStr.push_back("*");
+        }
+        else if (data.second == "/") {
+            synataxStr.push_back("/");
+        }
+        else if (data.second == "=") {
+            synataxStr.push_back("=");
+        }
+        else if (data.second == "!=") {
+            synataxStr.push_back("!=");
+        }
+        else if (data.second == "<") {
+            synataxStr.push_back("<");
+        }
+        else if (data.second == "<=") {
+            synataxStr.push_back("<=");
+        }
+        else if (data.second == ">") {
+            synataxStr.push_back(">");
+        }
+        else if (data.second == ">=") {
+            synataxStr.push_back(">=");
+        }
+        else if (data.second == "if") {
+            synataxStr.push_back("if");
+        }
+        else if (data.second == "then") {
+            synataxStr.push_back("then");
+        }
+        else {  //此时是非法字符
+            cout << "Invaild symbol!" << endl;
+            return false;
+        }
+    }
+    // 条件表达式文法暂时不做错误检测了/(ㄒoㄒ)/~~
+    // deque<string> tempStr = synataxStr;
+    synataxStr.push_back("#");
+
+    // for (int i = 0;i < (int)synataxStr.size();i++) {
+    //     cout << synataxStr[i] << " ";
+    // }
+    // cout << endl;
+    //构建预测分析表
+    unordered_map<string, int> hashmap = {
+        {"b",1},{"n",2},{"(",3},{")",4},{"+",5},{"-",6},{"*",7},{"/",8},
+        {"=",9},{"!=",10},{"<",11},{"<=",12},{">",13},{">=",14},{"odd",15},{"if",16},{"then",17},{"#",18},
+        {"S",1},{"T",2},{"E",3},{"R",4},{"I",5},{"O",6},{"F",7},{"A",8},{"M",9},{"P",10}
+    };
+    //构建终结符集合
+    unordered_set<string> hashset = {
+        "b","n","(",")","+","-","*","/","=","!=","<","<=",">",">=","odd","if","then","#"
+    };
+    vector<string> predictTable[32][32];
+    for (int i = 1;i <= 10;i++) {
+        for (int j = 1;j <= 18;j++) {
+            predictTable[i][j].push_back("ERROR");
+        }
+    }
+
+    predictTable[1][16].pop_back();predictTable[1][16].push_back("if");predictTable[1][16].push_back("T");predictTable[1][16].push_back("then");
+    predictTable[2][1].pop_back();predictTable[2][1].push_back("E");predictTable[2][1].push_back("P");predictTable[2][1].push_back("E");
+    predictTable[2][2].pop_back();predictTable[2][2].push_back("E");predictTable[2][2].push_back("P");predictTable[2][2].push_back("E");
+    predictTable[2][3].pop_back();predictTable[2][3].push_back("E");predictTable[2][3].push_back("P");predictTable[2][3].push_back("E");
+    predictTable[2][5].pop_back();predictTable[2][5].push_back("E");predictTable[2][5].push_back("P");predictTable[2][5].push_back("E");
+    predictTable[2][6].pop_back();predictTable[2][6].push_back("E");predictTable[2][6].push_back("P");predictTable[2][6].push_back("E");
+    predictTable[2][15].pop_back();predictTable[2][15].push_back("odd");predictTable[2][15].push_back("E");
+    predictTable[3][1].pop_back();predictTable[3][1].push_back("I");
+    predictTable[3][2].pop_back();predictTable[3][2].push_back("I");
+    predictTable[3][3].pop_back();predictTable[3][3].push_back("I");
+    predictTable[3][5].pop_back();predictTable[3][5].push_back("A");predictTable[3][5].push_back("I");predictTable[3][5].push_back("R");
+    predictTable[3][6].pop_back();predictTable[3][6].push_back("A");predictTable[3][6].push_back("I");predictTable[3][6].push_back("R");
+    predictTable[4][4].pop_back();predictTable[4][4].push_back("ε");
+    predictTable[4][5].pop_back();predictTable[4][5].push_back("A");predictTable[4][5].push_back("I");predictTable[4][5].push_back("R");
+    predictTable[4][6].pop_back();predictTable[4][6].push_back("A");predictTable[4][6].push_back("I");predictTable[4][6].push_back("R");
+    predictTable[4][9].pop_back();predictTable[4][9].push_back("ε");
+    predictTable[4][10].pop_back();predictTable[4][10].push_back("ε");
+    predictTable[4][11].pop_back();predictTable[4][11].push_back("ε");
+    predictTable[4][12].pop_back();predictTable[4][12].push_back("ε");
+    predictTable[4][13].pop_back();predictTable[4][13].push_back("ε");
+    predictTable[4][14].pop_back();predictTable[4][14].push_back("ε");
+    predictTable[4][17].pop_back();predictTable[4][17].push_back("ε");
+    predictTable[5][1].pop_back();predictTable[5][1].push_back("F");predictTable[5][1].push_back("O");
+    predictTable[5][2].pop_back();predictTable[5][2].push_back("F");predictTable[5][2].push_back("O");
+    predictTable[5][3].pop_back();predictTable[5][3].push_back("F");predictTable[5][3].push_back("O");
+    predictTable[6][4].pop_back();predictTable[6][4].push_back("ε");
+    predictTable[6][5].pop_back();predictTable[6][5].push_back("ε");
+    predictTable[6][6].pop_back();predictTable[6][6].push_back("ε");
+    predictTable[6][7].pop_back();predictTable[6][7].push_back("M");predictTable[6][7].push_back("F");predictTable[6][7].push_back("O");
+    predictTable[6][8].pop_back();predictTable[6][8].push_back("M");predictTable[6][8].push_back("F");predictTable[6][8].push_back("O");
+    predictTable[6][9].pop_back();predictTable[6][9].push_back("ε");
+    predictTable[6][10].pop_back();predictTable[6][10].push_back("ε");
+    predictTable[6][11].pop_back();predictTable[6][11].push_back("ε");
+    predictTable[6][12].pop_back();predictTable[6][12].push_back("ε");
+    predictTable[6][13].pop_back();predictTable[6][13].push_back("ε");
+    predictTable[6][14].pop_back();predictTable[6][14].push_back("ε");
+    predictTable[6][17].pop_back();predictTable[6][17].push_back("ε");
+    predictTable[7][1].pop_back();predictTable[7][1].push_back("b");
+    predictTable[7][2].pop_back();predictTable[7][2].push_back("n");
+    predictTable[7][3].pop_back();predictTable[7][3].push_back("(");predictTable[7][3].push_back("E");predictTable[7][3].push_back(")");
+    predictTable[8][5].pop_back();predictTable[8][5].push_back("+");
+    predictTable[8][6].pop_back();predictTable[8][6].push_back("-");
+    predictTable[9][7].pop_back();predictTable[9][7].push_back("*");
+    predictTable[9][8].pop_back();predictTable[9][8].push_back("/");
+    predictTable[10][9].pop_back();predictTable[10][9].push_back("=");
+    predictTable[10][10].pop_back();predictTable[10][10].push_back("!=");
+    predictTable[10][11].pop_back();predictTable[10][11].push_back("<");
+    predictTable[10][12].pop_back();predictTable[10][12].push_back("<=");
+    predictTable[10][13].pop_back();predictTable[10][13].push_back(">");
+    predictTable[10][14].pop_back();predictTable[10][14].push_back(">=");
+
+    // 进行LL(1)分析
+    stack<string> synataxSt;
+    // 初始化塞入#和E
+    synataxSt.push("#");synataxSt.push("S");
+    while (synataxSt.top() != "#" || synataxStr.front() != "#") {
+        string stCh = synataxSt.top();
+        string strCh = synataxStr.front();
+        // cout << strCh << "," << stCh << endl;
+        if (hashset.find(stCh) != hashset.end() && hashset.find(strCh) != hashset.end() && stCh == strCh) { //同为终结符且相等
+            synataxSt.pop();
+            synataxStr.pop_front();
+        }
+        else if (hashset.find(stCh) != hashset.end() && hashset.find(strCh) != hashset.end() && stCh != strCh) {    //同为终结符且不等
+            cout << synataxSt.top() << " " << synataxStr.front() << endl;
+            cout << "终结符不一致" << endl;
+            cout << "Synatax Error, Please Check." << endl;
+            return false;
+        }
+        else {  //从分析预测表中取出推理字符串
+            vector<string> expression = predictTable[hashmap[stCh]][hashmap[strCh]];
+            if (expression.front() == "ERROR") {    //踩到预测表中空的项
+                cout << synataxSt.top() << " " << synataxStr.front() << endl;
+                cout << "空预测表" << endl;
+                cout << "Synatax Error, Please Check." << endl;
+                return false;
+            }
+            else if (expression.front() == "ε") {   //处理产生式右部是空的情况
+                synataxSt.pop();
+            }
+            else {  //正常推理
+                synataxSt.pop();
+                for (int i = (int)expression.size() - 1;i >= 0;i--) {
+                    synataxSt.push(expression[i]);
+                }
+            }
+        }
+        // //cout << synataxSt.size() << "," << synataxStr.size() << endl;
+        // if ((synataxSt.top() == '#' && synataxStr.front() != '#') || (synataxSt.top() != '#' && synataxStr.front() == '#')) {
+        //     cout << synataxSt.size() << "," << synataxStr.size() << endl;
+        //     cout << "Synatax Error, Please Check." << endl;
+        //     errorSearch(tempStr);
+        //     return;
+        // }
+        if (synataxSt.top() == "#" && synataxStr.front() != "#") {
+            cout << "预测分析栈先空了" << endl;
+            cout << "Synatax Error, Please Check." << endl;
+            return false;
+        }
+    }
+    cout << "Synatax is Correct." << endl;
+    return true;
+}
+
+
 
 // predictTable[1][5] = "E->AIR";
 // predictTable[1][6] = "E->AIR";
